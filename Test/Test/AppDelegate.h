90a85c8137364870232468dc7e332534f29bb203
Cqiaoyu <1250578320@qq.com>
2016-05-16 16:46:27 +0800

    first commit

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Kenway on 16/5/16.
+//  Copyright © 2016年 Kenway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Kenway on 16/5/16.
+//  Copyright © 2016年 Kenway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
